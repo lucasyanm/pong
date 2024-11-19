906a064e03c974803c21ac372323c97b57e56b9f
Lucas Carvalho <lucasyancarvalho@gmail.com>
2024-11-18 21:02:25 -0300

    feat:  window resize

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -1,7 +1,13 @@
 #include <windows.h>
 
+//callback
 LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+LPVOID bufferMemory; //LPVOID same as void*
+int bufferWidth;
+int bufferHeight;
+
+//win main
 bool running = true;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
@@ -51,11 +57,28 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     LRESULT result = 0;
+    int bufferSize;
 
     switch (uMsg) {
         case WM_CLOSE:
         case WM_DESTROY:
             running = false;
+            break;
+        case WM_SIZE:
+            RECT rectangle;
+
+            GetClientRect(hwnd, &rectangle);
+
+            bufferWidth = rectangle.right - rectangle.left;
+            bufferHeight = rectangle.bottom - rectangle.top;
+
+            bufferSize = bufferWidth * bufferHeight * sizeof(unsigned int);
+
+            if(bufferMemory)
+                VirtualFree(bufferMemory, 0, MEM_RELEASE);
+
+            bufferMemory = VirtualAlloc(NULL, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
             break;
         default:
             result = DefWindowProc(hwnd, uMsg, wParam, lParam);
