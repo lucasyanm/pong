d8f8c2a8a23006b431918aedb25920c9f968b50d
Lucas Carvalho <lucasyancarvalho@gmail.com>
2024-11-18 21:58:00 -0300

    feat: bit map info

--- win32_platform.cpp
+++ win32_platform.cpp
@@ -3,9 +3,14 @@
 //callback
 LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-LPVOID bufferMemory; //LPVOID same as void*
-int bufferWidth;
-int bufferHeight;
+struct RenderState {
+    LPVOID memory; //LPVOID same as void*
+    int width;
+    int height;
+
+    BITMAPINFO bitMapInfo;
+};
+RenderState renderState;
 
 //win main
 bool running = true;
@@ -33,6 +38,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         hInstance, 
         NULL);
 
+    HDC deviceContext = GetDC(window);
+
     while(running)
     {
         // Input
@@ -48,7 +55,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
             DispatchMessageW(&message);
         }
         // Update
+        unsigned int* pixel = (unsigned int*)renderState.memory;
+
+        for(int i = 0; i < renderState.height; i++) {
+            for(int j = 0; j < renderState.width; j++) {
+                *pixel++ = i + j;
+            }
+        }
         // Render
+        StretchDIBits(
+            deviceContext,
+            0, 0, renderState.width, renderState.height,
+            0, 0, renderState.width, renderState.height,
+            renderState.memory,
+            &renderState.bitMapInfo,
+            DIB_RGB_COLORS,
+            SRCCOPY);
     }
 
     return 0;
@@ -69,15 +91,22 @@ LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 
             GetClientRect(hwnd, &rectangle);
 
-            bufferWidth = rectangle.right - rectangle.left;
-            bufferHeight = rectangle.bottom - rectangle.top;
+            renderState.width = rectangle.right - rectangle.left;
+            renderState.height = rectangle.bottom - rectangle.top;
 
-            bufferSize = bufferWidth * bufferHeight * sizeof(unsigned int);
+            bufferSize = renderState.width * renderState.height * sizeof(unsigned int);
 
-            if(bufferMemory)
-                VirtualFree(bufferMemory, 0, MEM_RELEASE);
+            if(renderState.memory)
+                VirtualFree(renderState.memory, 0, MEM_RELEASE);
 
-            bufferMemory = VirtualAlloc(NULL, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            renderState.memory = VirtualAlloc(NULL, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+            renderState.bitMapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+            renderState.bitMapInfo.bmiHeader.biWidth = renderState.width;
+            renderState.bitMapInfo.bmiHeader.biHeight = -renderState.height;
+            renderState.bitMapInfo.bmiHeader.biPlanes = 1;
+            renderState.bitMapInfo.bmiHeader.biBitCount = 32;
+            renderState.bitMapInfo.bmiHeader.biCompression = BI_RGB;
 
             break;
         default:
