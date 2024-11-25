4adab7202056148a8f67d48f8c50b545e7662899
Lucas Carvalho <lucasyancarvalho@gmail.com>
2024-11-25 18:04:21 -0300

    feat: hold button validation

--- include/macros.h
+++ include/macros.h
@@ -2,10 +2,11 @@
 #define GLOBALVAR static
 
 #define isPressed(b) (input.buttons[b].pressed && input.buttons[b].changed)
+#define isHold(b) (input.buttons[b].pressed && !input.buttons[b].changed)
 #define isReleased(b) (!input.buttons[b].pressed && input.buttons[b].changed)
 
 #define processButton(buttonType, vkCode, pressed)\
     case vkCode:\
+        input.buttons[buttonType].changed = pressed != input.buttons[buttonType].pressed;\
         input.buttons[buttonType].pressed = pressed;\
-        input.buttons[buttonType].changed = true;\
         break;
