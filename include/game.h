37b5841d0d95a5c31dbdc8824317acb018265b39
Lucas Carvalho <lucasyancarvalho@gmail.com>
2024-11-25 20:58:38 -0300

    refact: player struct and move const to game.h

--- include/game.h
+++ include/game.h
@@ -4,6 +4,17 @@
 #include "input.h"
 #include "renderer.h"
 
+GLOBALVAR const UINT mainColor = 0xebebeb;
+GLOBALVAR const UINT secColor = 0x1c1c1c;
+
+GLOBALVAR const float playerSpeed = 100.f; //units (pixels) per second
+
+struct Player {
+    float playerPos = 0.f;
+    float playerDerivativePos = 0.f;
+    float playerDerivativeDerivativePos = 0.f;
+};
+
 void simulateGame(
     const Input& input, 
     const float& deltaTimeInSeconds, 
--- src/game.cpp
+++ src/game.cpp
@@ -1,11 +1,7 @@
 #include "../include/game.h"
 
-float playerPos, playerDerivativePos;
-
-const UINT mainColor = 0xebebeb;
-const UINT secColor = 0x1c1c1c;
-
-GLOBALVAR const float playerSpeed = 100.f; //units (pixels) per second
+Player player1;
+Player player2;
 
 // BUG: Delay to start player movement
 void simulateGame(
@@ -17,20 +13,20 @@ void simulateGame(
     clearScreen(renderState, mainColor);
     renderRect(renderState, 0, 0, 85, 45, secColor);
 
+    player1.playerDerivativeDerivativePos = 0.f;
     //movement
-    float playerDerivativeDerivativePos = 0.f;
     if (isHold(Button::DOWN))
     {
-        playerDerivativeDerivativePos -= 2000;
+        player1.playerDerivativeDerivativePos -= 2000;
     };
     if (isHold(Button::UP)) {
-        playerDerivativeDerivativePos += 2000;
+        player1.playerDerivativeDerivativePos += 2000;
     };
 
-    playerDerivativeDerivativePos -= playerDerivativePos * 10.f;
+    player1.playerDerivativeDerivativePos -= player1.playerDerivativePos * 10.f;
 
-    playerPos = playerPos + playerDerivativePos * deltaTimeInSeconds + playerDerivativeDerivativePos * deltaTimeInSeconds * deltaTimeInSeconds * .5f;
-    playerDerivativePos = playerDerivativePos + playerDerivativeDerivativePos * deltaTimeInSeconds;
+    player1.playerPos = player1.playerPos + player1.playerDerivativePos *deltaTimeInSeconds + player1.playerDerivativeDerivativePos * deltaTimeInSeconds * deltaTimeInSeconds * .5f;
+    player1.playerDerivativePos = player1.playerDerivativePos + player1.playerDerivativeDerivativePos * deltaTimeInSeconds;
 
     //ball
     renderRect(renderState, 0, 0, 1, 1, mainColor);
@@ -38,5 +34,6 @@ void simulateGame(
     //player Right
     renderRect(renderState, 80, 0, 2.5, 12, mainColor);
     //player Left
-    renderRect(renderState, -80, playerPos, 2.5, 12, mainColor);
+    renderRect(renderState, -80, player1.playerPos, 2.5, 12, mainColor);
 }
+
